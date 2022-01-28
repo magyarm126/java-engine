b0db805886053cfaf7b7ac8c9f85c5315110bb19
magyarm126 <magyarmate126@gmail.com>
2022-01-29 00:32:01 +0100

    - Gradle application plugin added - Upgraded versions

---
+++ src/main/java/engine/Main.java
@@ -0,0 +1,14 @@
+package engine;
+
+/***
+ * Main class
+ */
+public class Main {
+    /***
+     * Entry point of the application
+     * @param args Application arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
