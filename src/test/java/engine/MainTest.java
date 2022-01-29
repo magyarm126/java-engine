f3090147a90c1050e35885bcb250bd7a61f6ab07
magyarm126 <magyarmate126@gmail.com>
2022-01-29 14:05:02 +0100

    - Sample jupiter test added

---
+++ src/test/java/engine/MainTest.java
@@ -0,0 +1,12 @@
+package engine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Assertions.assertEquals("Sample test works", "Sample test works");
+    }
+}
